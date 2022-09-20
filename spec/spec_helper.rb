c94a0f2e4e3a171e4dab08398349afb8a59680e2
Daniel Loy <daniel.loy@ioki.com>
2022-09-20 16:04:57 +0200

    Let rspec run all specs if all filtered out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,10 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
+  # Guard tries to run only tests with the focus tag, if this would filter out
+  # all specs, run all tests.
+  config.run_all_when_everything_filtered = true
+
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
