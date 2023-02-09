bb8cb8b80f75ac01dcfd63828d09beb55023259b
Patrick Kocurek <patrick.kocurek@ioki.com>
2023-02-09 09:25:28 +0100

    Rename attributes in spec

--- spec/lib/airship/api/named_user_attributes_update_spec.rb
+++ spec/lib/airship/api/named_user_attributes_update_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Airship::Api::NamedUserAttributesUpdate do
 
   let(:operation_params) do
     {
-      app_key:                app_key,
-      token:                  token,
-      named_user_id:          named_user_id,
-      attributes_with_values: { first_name: first_name, last_name: last_name },
-      updated_at:             updated_at
+      app_key:       app_key,
+      token:         token,
+      named_user_id: named_user_id,
+      attributes:    { first_name: first_name, last_name: last_name },
+      updated_at:    updated_at
     }
   end
 
